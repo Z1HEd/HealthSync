45b3f12ff4d64b8a8e317846bc24b2b49e42254a
ZiHEd <ziihed@gmail.com>
2025-03-31 17:03:45 +0200

    horray it works

--- main.cpp
+++ main.cpp
@@ -13,9 +13,30 @@ initDLL
 float getEntityHealth(Entity* entity) {
     if (entity->getName() == "Spider") return ((EntitySpider*)entity)->health;
     if (entity->getName() == "Butterfly")return ((EntityButterfly*)entity)->health;
-    if (entity->getName() == "Player" && ((EntityPlayer*)entity)->ownedPlayer != nullptr) return ((EntityPlayer*)entity)->ownedPlayer->health;
-    Console::printLine("Unknown entity or empty EntityPlayer::ownedPlayer in getHealth ");
+    if (entity->getName() == "Player") return ((EntityPlayer*)entity)->player->health;
     return 0;
+}
+
+void sendPacketInRegion(WorldServer* world, const fdm::stl::string& packet, const nlohmann::json& data, const glm::vec4& origin)
+{
+    for (int x = -1; x <= 1; ++x)
+        for (int z = -1; z <= 1; ++z)
+            for (int w = -1; w <= 1; ++w)
+            {
+                Chunk* chunk = world->getChunkFromCoords(origin.x + 8 * x, origin.z + 8 * z, origin.w + 8 * w);
+
+                if (!chunk) continue;
+
+                for (auto& entity : chunk->entities)
+                {
+                    if (entity->getName() != "Player") continue;
+                    if (!world->entityPlayerIDs.contains(entity->id)) continue;
+
+                    uint32_t handle = world->entityPlayerIDs.at(entity->id)->handle;
+
+                    JSONData::sendPacketClient(world, packet, data, handle);
+                }
+            }
 }
 
 void onHealthChanged(WorldServer* server, Entity* entity) {
@@ -23,26 +44,24 @@ void onHealthChanged(WorldServer* server, Entity* entity) {
 
     healthData["entityId"] = stl::uuid::to_string(entity->id);
     healthData["health"] = getEntityHealth(entity);
+    
 
-    JSONData::broadcastPacket(server, "entityHealthSync", healthData);
-}
-
-void onHealthChanged(WorldServer* server, Player* player) {
-    nlohmann::json healthData;
-
-    healthData["entityId"] = stl::uuid::to_string(player->EntityPlayerID);
-    healthData["health"] = player->health;
-
-    JSONData::broadcastPacket(server, "entityHealthSync", healthData);
+    sendPacketInRegion(server, "zihed.healthsync.entityHealthSync", healthData, entity->getPos());
 }
 
 // Update health on client
 
 void setEntityHealth(Entity* entity, float health) {
+
+
     if (entity->getName()=="Spider") ((EntitySpider*)entity)->health = health;
     else if (entity->getName() == "Butterfly")((EntityButterfly*)entity)->health = health;
-    else if (entity->getName() == "Player" && ((EntityPlayer*)entity)->ownedPlayer!=nullptr) ((EntityPlayer*)entity)->ownedPlayer->health = health;
-    else Console::printLine("Unknown entity or empty EntityPlayer::ownedPlayer in setHealth ");
+    else if (entity->getName() == "Player") {
+        if (StateGame::instanceObj.player.EntityPlayerID == entity->id)
+            StateGame::instanceObj.player.health = health;
+        ((EntityPlayer*)entity)->player->health = health;
+    }
+
 }
 
 void onEntityHealthSync(fdm::WorldClient* world, fdm::Player* player, const nlohmann::json& data) {
@@ -58,15 +77,12 @@ void onEntityHealthSync(fdm::WorldClient* world, fdm::Player* player, const nloh
 $hook(void, StateIntro, init, StateManager& s)
 {
     original(self, s);
-
-    Console::printLine("Initializing HealthSync mod");
-
     // initialize opengl stuff
     glewExperimental = true;
     glewInit();
     glfwInit();
 
-    JSONData::SCaddPacketCallback("entityHealthSync", onEntityHealthSync);
+    JSONData::SCaddPacketCallback("zihed.healthsync.entityHealthSync", onEntityHealthSync);
 }
 
 // Send packets when stuff happens
@@ -83,8 +99,7 @@ $hook(void, Player, update, World* world, double dt, EntityPlayer* entityPlayer)
     int thisUpdateHealth = self->health;
 	
     if (thisUpdateHealth != lastUpdateHealth) {
-        Console::printLine("PlayerHealthChanged! ");
-        onHealthChanged(server, self);
+        onHealthChanged(server, entityPlayer);
     }
     lastUpdateHealth = thisUpdateHealth;
 	
@@ -97,8 +112,6 @@ $hook(void, EntitySpider, takeDamage, float damage, World* world) {
 
     WorldServer* server = (WorldServer*)(world);
 
-
-    Console::printLine("SpiderHealthChanged! ");
     onHealthChanged(server, self);
 }
 
@@ -109,8 +122,5 @@ $hook(void, EntityButterfly, takeDamage, float damage, World* world) {
 
     WorldServer* server = (WorldServer*)(world);
 
-    
-
-    Console::printLine("ButterflyHealthChanged! ");
     onHealthChanged(server, self);
 }
